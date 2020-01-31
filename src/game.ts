98abde28da094a5ead2b4316b52a7b4102b2730d
Josh Biddick <josh@ricado.co.nz>
2020-02-01 11:33:01 +1300

    initialize map with ground tiles

--- src/game.ts
+++ src/game.ts
@@ -5,9 +5,35 @@ export default class MainScene extends Phaser.Scene {
     super('mainScene');
   }
 
-  preload() {}
+  preload() {
+    // map made with Tiled in JSON format
+    this.load.tilemapTiledJSON('map', 'assets/map.json');
+    // tiles in spritesheet
+    this.load.spritesheet('tiles', 'assets/tiles.png', {
+      frameWidth: 70,
+      frameHeight: 70,
+    });
+    // simple coin image
+    this.load.image('coin', 'assets/coinGold.png');
+    // player animations
+    this.load.atlas('player', 'assets/player.png', 'assets/player.json');
+  }
 
-  create() {}
+  create() {
+    // load the map
+    const map = this.make.tilemap({ key: 'map' });
+
+    // tiles for the ground layer
+    const groundTiles = map.addTilesetImage('tiles');
+    // create the ground layer
+    const groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
+    // the player will collide with this layer
+    groundLayer.setCollisionByExclusion([-1]);
+
+    // set the boundaries of our game world
+    this.physics.world.bounds.width = groundLayer.width;
+    this.physics.world.bounds.height = groundLayer.height;
+  }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
