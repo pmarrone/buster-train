94bd2af09a9590cebebf4dac0e61946250ba282a
Matias Iacono <matias.iacono@gmail.com>
2020-02-01 11:52:45 +1300

    removing comments

--- src/game.ts
+++ src/game.ts
@@ -6,60 +6,33 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    // map made with Tiled in JSON format
     this.load.tilemapTiledJSON('map', 'assets/map.json');
-    // tiles in spritesheet
     this.load.spritesheet('tiles', 'assets/tiles.png', {
       frameWidth: 70,
       frameHeight: 70,
     });
-    // simple coin image
+
     this.load.image('coin', 'assets/coinGold.png');
-    // player animations
     this.load.atlas('player', 'assets/player.png', 'assets/player.json');
   }
 
   create() {
-    // load the map
     const map = this.make.tilemap({ key: 'map' });
-
-    // tiles for the ground layer
     const groundTiles = map.addTilesetImage('tiles');
-    // create the ground layer
     const groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
-    // the player will collide with this layer
+
     groundLayer.setCollisionByExclusion([-1]);
 
-    // set the boundaries of our game world
     this.physics.world.bounds.width = groundLayer.width;
     this.physics.world.bounds.height = groundLayer.height;
     this.load.atlasXML('sprites', 'assets/spritesheet_complete.png', 'assets/spritesheet_complete.xml');
+
+    var player = this.physics.add.sprite(200, 30, 'player'); 
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(groundLayer, player);
   }
-
-  // create() {
-  //   var atlasTexture = this.textures.get('sprites');
-  //   var frames = atlasTexture.getFrameNames();
-
-  //   this.anims.create({
-  //       key: 'walk',
-  //       frames: this.anims.generateFrameNames('player', { prefix: 'man_walk', start: 1, end: 2, zeroPad: 0 }),
-  //       frameRate: 10,
-  //       repeat: 0
-  //   });
-
-  //   var player = this.physics.add.sprite(200, 200, 'sprites');
-  //   player.setBounce(0.2);
-  //   player.setCollideWorldBounds(true);
-  //   player.play("walk");
-
-  //   console.log(frames);
-    /*for (var i = 0; i < frames.length; i++) {
-      var x = Phaser.Math.Between(0, 800);
-      var y = Phaser.Math.Between(0, 600);
-
-      this.add.image(x, y, 'sprites', frames[i]);
-    }*/
-//   }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
