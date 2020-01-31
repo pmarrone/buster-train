cf521b2c502b39ff1c2683935df83f96f975bd8c
Matias Iacono <matias.iacono@gmail.com>
2020-02-01 10:54:55 +1300

    cleaning up

--- src/game.ts
+++ src/game.ts
@@ -1,34 +1,16 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene {
+export default class MainScene extends Phaser.Scene {
   constructor() {
-    super('demo');
+    super('mainScene');
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-    this.load.image('libs', 'assets/libs.png');
-    this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-    this.load.glsl('stars', 'assets/starfields.glsl.js');
+
   }
 
   create() {
-    this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
-    this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-    this.add.image(400, 300, 'libs');
-
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1,
-    });
   }
 }
 
@@ -37,7 +19,14 @@ const config = {
   backgroundColor: '#125555',
   width: 800,
   height: 600,
-  scene: Demo,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: {y: 500},
+        debug: false
+    }
+  },
+  scene: MainScene
 };
 
 const game = new Phaser.Game(config);
