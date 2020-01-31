553a6e6b02f54bf442548491c42441fb51ac67b3
Matias Iacono <matias.iacono@gmail.com>
2020-02-01 11:28:17 +1300

    algo

--- src/game.ts
+++ src/game.ts
@@ -6,20 +6,32 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.atlasXML('cars', 'assets/cars.png', 'assets/cars.xml');
+    this.load.atlasXML('sprites', 'assets/spritesheet_complete.png', 'assets/spritesheet_complete.xml');
   }
 
   create() {
-    var atlasTexture = this.textures.get('cars');
+    var atlasTexture = this.textures.get('sprites');
     var frames = atlasTexture.getFrameNames();
 
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNames('player', { prefix: 'man_walk', start: 1, end: 2, zeroPad: 0 }),
+        frameRate: 10,
+        repeat: 0
+    });
+
+    var player = this.physics.add.sprite(200, 200, 'sprites');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.play("walk");
+
     console.log(frames);
-    for (var i = 0; i < frames.length; i++) {
+    /*for (var i = 0; i < frames.length; i++) {
       var x = Phaser.Math.Between(0, 800);
       var y = Phaser.Math.Between(0, 600);
 
-      this.add.image(x, y, 'cars', frames[i]);
-    }
+      this.add.image(x, y, 'sprites', frames[i]);
+    }*/
   }
 }
 
