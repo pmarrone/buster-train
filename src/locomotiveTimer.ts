c72cd239a424941c81dc85a7bd65ade5a832ba3f
Matias Iacono <matias.iacono@gmail.com>
2020-02-02 11:02:57 +1300

    lala

--- src/locomotiveTimer.ts
+++ src/locomotiveTimer.ts
@@ -16,17 +16,19 @@ class LocomotiveTimer {
     var energyBar = this.scene.add.sprite(energyContainer.x + 46, energyContainer.y, "timeBar");
 
     energyContainer.setScrollFactor(0);
+    energyBar.setOrigin(1, 0.5);
     energyBar.setScrollFactor(0);
 
     /*this.energyMask = this.scene.add.sprite(energyBar.x, energyBar.y, "timeBar");
     this.energyMask.visible = false;
-    this.energyMask.setScrollFactor(0);*/
-    //energyBar.mask = new Phaser.Display.Masks.BitmapMask(this.scene, this.energyMask);
+    this.energyMask.setScrollFactor(0);
+    energyBar.mask = new Phaser.Display.Masks.BitmapMask(this.scene, this.energyMask);*/
 
     this.timer = this.scene.time.addEvent({
         delay: 500,
         callback: function() {
             var stepWidth = energyBar.displayWidth - (energyBar.displayWidth / 60);
+            energyBar.setScale(0.5,1);
             //this.energyMask.x -= stepWidth;
             //energyBar.setW(stepWidth);
         },
